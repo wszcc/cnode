a210d5f43441c1939094669dc7a65b896c51a9c6
wszcc <2149857139@qq.com>
2020-12-21 00:41:33 +0800

    初始化项目

---
+++ src/store/reducer.ts
@@ -0,0 +1,14 @@
+const initState={
+
+}
+interface actionType{
+    type:string,
+    payload:any
+}
+export function (state:any,action:actionType){
+    switch (action.type) {
+        
+        default:
+            break;
+    }
+}
